308397bc43940ead2f93e87bb68c8aee1b536146
punkatux <punkatux@gmail.com>
2012-09-14 00:25:59 +0200

    datatypes.h - typy U8, U16, U32, U64 a S8, S16, S32, S64

--- datatypes.h
+++ datatypes.h
@@ -6,4 +6,15 @@
 
 typedef enum {FALSE = 0, TRUE = 1} bool;
 
+#include <stdint.h>
+
+typedef uint8_t U8;
+typedef uint16_t U16;
+typedef uint32_t U32;
+typedef uint64_t U64;
+typedef int8_t S8;
+typedef int16_t S16;
+typedef int32_t S32;
+typedef int64_t S64;
+
 #endif //DATATYPES_H_
