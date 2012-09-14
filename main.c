57df25329b5ee54e92deda545f4bb4d34ec803fd
punkatux <punkatux@gmail.com>
2012-09-14 15:44:58 +0200

    test hexdump() a hton16(), ntoh16()

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+#include "macros.h"
 #include "endianess.h"
 
 int main(void)
 {
-	printf("%s endian\n", is_little_endian() ? "Little" : "Big");
+	U16 port = 10000;
+
+	hexdump(port);
+
+	hexdump(hton16(port));
+	hexdump(ntoh16(hton16(port)));
 
 	return EXIT_SUCCESS;
 }
