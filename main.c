ea85ff66204d5814e74a5510c7bce619d2074da4
punkatux <punkatux@gmail.com>
2012-09-16 16:56:08 +0200

    netif_write() test

--- main.c
+++ main.c
@@ -3,14 +3,19 @@
 
 #include "datatypes.h"
 #include "macros.h"
+#include "netif.h"
 #include "arp.h"
 
 int main(void)
 {
+	netif_init();
+
 	static U8 buffer[42];
 	arp_request(buffer, (ip_t) {192, 168, 1, 201});
-	
+
 	hexdump_var(buffer);
+
+	netif_write(buffer, sizeof(buffer));
 
 	return EXIT_SUCCESS;
 }
